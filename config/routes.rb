226147998eb3658b1c767c2c416edbd801a46eb4
Emily_rrz <emily.zheng828@gmail.com>
2016-05-11 20:01:13 +0100

    create homecontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'contact' => "home#contact"
+  get 'aboutus' => "home#aboutus"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
