e992b6922393781596388ed54a21937df86afd21
vagrant <vagrant@heroku-cedar.(none)>
2016-11-15 20:26:08 +0000

    name section added for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'contact' => "home#contact"
   get 'aboutus' => "home#aboutus"
 
